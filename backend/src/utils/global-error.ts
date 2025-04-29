dd1091d2453234fceb4139b22c54650b4117298f
TwitchBluelow <fabriziomonaco@outlook.de>
2025-04-29 13:50:01 +0200

    chore: remove authentication middleware and related error handling

--- backend/src/utils/global-error.ts
+++ backend/src/utils/global-error.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import { TokenExpiredError } from 'jsonwebtoken';
 import { ZodError } from 'zod';
 
 export function globalErrorHandler(
@@ -12,13 +11,6 @@ export function globalErrorHandler(
   if (err instanceof ZodError) {
     res.status(400).json({
       errors: err.errors,
-    });
-    return;
-  }
-
-  if (err instanceof TokenExpiredError) {
-    res.status(401).json({
-      errors: ['Token expired'],
     });
     return;
   }
